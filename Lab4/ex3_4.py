ed7850861e34ecb386a30a53592e221d1b7c5658
Ethan <efoust@hawaii.edu>
2024-09-20 12:18:54 -1000

    lab4last

--- Lab4/ex3_4.py
+++ Lab4/ex3_4.py
@@ -1,9 +1,17 @@
 #3.4 edit list
-respones = [5,7,3,8]
+#respones = [5,7,3,8]
 
-respones.append(0)
+#respones.append(0)
 #respones.insert(2,6)
-respones = respones[0:6] + [6] + respones[2:]
-respones.sort()
-respones.remove(0)
-print(respones)
+#respones = respones[0:6] + [6] + respones[2:]
+#respones.sort()
+#respones.remove(0)
+#print(respones)
+
+
+#using tuples
+
+
+studentIDs = (1012, 1035, 1021, 1053)
+studentIDs.append(1011)
+print(studentIDs)
--- Lab4/ex4.py
+++ Lab4/ex4.py
@@ -3,4 +3,3 @@
 studentIDs = (1012, 1035, 1021, 1053)
 newSudentIDs = (1012, 1035, 1021, 1053) + (1011,)
 print(newSudentIDs)
-
