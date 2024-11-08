5f2f3047884b12d904ed64bbd5fbe3e0b5b35139
Ethan <efoust@hawaii.edu>
2024-11-08 12:54:01 -1000

    lab13/A3workshop

--- Lab13/quizAppFLASK.py
+++ Lab13/quizAppFLASK.py
@@ -1,7 +1,7 @@
 #qiz app using flask
 #Create a simple log in page 
 from flask import Flask, render_template, request, redirect, url_for
-
+import json
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,20 +11,32 @@ def home():
 #asking a question
 @app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
+    global question_num, score
     if request.method == 'POST':
         # Logic to capture the user’s answers and redirect to the result page
-        return redirect(url_for('result'))
+        #if ____
+        score += 1
+        return redirect(url_for('result',score=score))
     # Load the question and options to display
-    return render_template('quiz.html')  # Displays the question and options
+    question_num += 1
+    return render_template('quiz.html',
+                           num=question_num, 
+                           question=question_list[question_num-1][0],options=question_list[question_num-1][1] )  # Displays the question and options
 
 
 @app.route('/result')
 def result():
     # Calculate and display the user's score
-    score = 1  # Example score for demonstration
     return render_template('result.html', score=score)
 
+#Load the question file and convert it to a list
+question_file = open("questions.json")
+questions = json.load(question_file)
+question_list = list(questions.items())
 
+#housekeeping variables
+score = 0
+question_num = 0
 
 USERS = {
     "port": "port123",
