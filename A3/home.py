bcbab4008d1c5b7465ec791ddb696061c3f85e55
Ethan <efoust@hawaii.edu>
2024-11-22 15:59:31 -1000

    finalPushA3(w/ comments)

--- A3/home.py
+++ A3/home.py
@@ -1,4 +1,4 @@
-# Quiz app using Flask 
+# Quiz app 
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import json
 from datetime import datetime
@@ -6,12 +6,13 @@ from datetime import datetime
 app = Flask(__name__)
 
 app.secret_key = 'ethan'
-
+#load user file in order to validate the login page
 def load_users():
     with open('user.json') as user_file:
         users = json.load(user_file)
     return users["users"]
 
+#home page
 @app.route("/")
 def home():
     global question_num, score, start_time
@@ -20,7 +21,8 @@ def home():
     start_time = datetime.now()
     return render_template('home.html')
 
-
+#login route displays provides content for login page, checks if username matches an established username in the user.json file
+#saves what the user has submitted into a variable. if the login info is correct, move to the home pagee.
 @app.route('/login', methods = ['GET','POST'])
 def login():
     global username, password
@@ -33,27 +35,31 @@ def login():
             if user['username'] ==  username and user['password'] == password:
                 session['username'] = username
                 return redirect(url_for('home'))
-        #flash('Invalid credentials, please try again')
     return render_template('login.html')
 
+
+
+#logout back to home page, clear username from session
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('home'))
-
+#Quiz route
 @app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
     if 'username' not in session:
         return redirect(url_for('login'))
     else:
-       
+       #make global and reset variables
         global question_num, score, hint
         result = None
         hint = None
+        #define variables for result 
         correct_message = 'Nice! Your answer was Correct!'
         incorrect_message = 'Sorry! Your answer was Incorrect!'
-
+        
         if request.method == 'POST':
+            #check if hint button is pressed
             if request.form.get("action") == 'hint':
                 hint= question_list[question_num].get("hint","No hint available")
             elif request.form.get('next'):
@@ -61,7 +67,6 @@ def quiz():
                 if question_num >= len(question_list):
                     return redirect(url_for('result',score=score))
             else:
-                # Logic to capture the user’s answers and redirect to the result page
                 user_answer = request.form.get("answer")
                 correct_answer = question_list[question_num]["answer"]
                 if user_answer == correct_answer:
@@ -81,12 +86,12 @@ def quiz():
 @app.route('/result')
 def result():
     global score_percentage, time_taken, displayed_time, scores_data
-
+    #check if user logged in
     if 'username' not in session:
         return redirect(url_for('login'))
     
    
-
+    #calculate results for the results page and to save to scores.json
 
     end_time = datetime.now() 
     time_taken = end_time - start_time
@@ -102,7 +107,7 @@ def result():
     score_percentage = score/question_num * 100
     score_percentage = int(score_percentage)
 
-
+    #display how long the quiz took with words
     if(formatted_hours >= 1):
         displayed_time = f"You took {formatted_hours} hours and {formatted_minutes} minutes and {formatted_seconds} seconds to complete this quiz."
     elif(formatted_minutes >= 1):
@@ -110,6 +115,7 @@ def result():
     else:
         displayed_time = f"You took {formatted_seconds} seconds to complete this quiz."
 
+    #format display for scores.json and save into a variable
     scores_data = {
                         "username": session['username'],
                         "score": score,
@@ -118,7 +124,7 @@ def result():
                         "total_questions": len(question_list)
                         
     }
-
+    #load scores.json and pass the data from scores_data into the file. saving scores
     try:
         with open('scores.json', 'r+') as scores_file:
             try:
@@ -134,10 +140,7 @@ def result():
 
     return render_template('result.html', score=score, question_num = question_num, score_percentage = score_percentage, displayed_time = displayed_time, time_taken = time_taken)
 
-# Load the question file and convert it to a list
-#question_file = open("questions.json")
-#questions = json.load(question_file)
-#question_list = list(questions.items())
+#convert questions.json into a list of questions
 with open("questions.json") as question_file:
     question_list = json.load(question_file)["questions"]
 
